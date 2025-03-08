918376d1d409096c3b0c300c69feedb487b50556
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-03-08 17:48:38 +0800

    chore: update deps

--- playground/uno.config.ts
+++ playground/uno.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig, presetIcons, presetUno } from 'unocss'
+import { presetWind } from '@unocss/preset-wind3'
+import { defineConfig, presetIcons } from 'unocss'
 import presetAnimations from 'unocss-preset-animations'
 import { builtinColors, presetShadcn } from 'unocss-preset-shadcn'
 
 export default defineConfig({
   presets: [
-    presetUno(),
+    presetWind(),
     presetIcons({
       scale: 1.3,
     }),
